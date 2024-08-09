8884b947805d22bb2043a3762a4637e59db4d4df
riplblr32 <itsupport1@rajamane.com>
2024-08-09 15:22:02 +0530

    check test user karthik(private)

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ app.use('/api/auth', authRoutes);
 app.use('/api/tickets', ticketRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port https://192.168.1.32:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port http://192.168.1.32:${PORT}`));
